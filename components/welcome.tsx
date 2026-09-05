c81eadfc34e49d054b42192a9efdd2cb1387c323
amilcarj <amilcar.javier@nyu.edu>
2026-09-05 02:46:19 -0400

    typo fix

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -3,7 +3,7 @@ const Welcome = () => {
     <section className="flex flex-col items-center justify-center w-full pb-3">
       <p>
         The Gamma Alpha Graduate/Professional Chapter of La Unidad Latina
-        Lambda, Upsilon Lambda Fraternity, Inc. are partnering with Voto Latino
+        Lambda, Upsilon Lambda Fraternity, Inc. is partnering with Voto Latino
         to encourage Black & Brown people to participate in local and national
         elections and increase civic engagement in the communities we serve.
       </p>
